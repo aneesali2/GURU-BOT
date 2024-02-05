3af845de4f6d42e1c2e60ef140b66c3ad2f21f00
aneesali2 <158823460+aneesali2@users.noreply.github.com>
2024-02-05 08:52:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923202567970', 'king botz', true],
+  ['6281255369012', 'Anees', true], 
   [''] 
 ] //Number of owners
 
